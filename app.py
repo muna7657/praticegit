0a720e3534cb18eba2f0dbbae6dfefd0ea40a186
muna7657 <79190448+muna7657@users.noreply.github.com>
2022-05-21 15:58:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import os
 from flask import Flask
 app = Flask(__name__)
-
-
 @app.route("/")
 def main():
-    return "welcome"
-
-@app.route("/how are you")
+  return "welcome!"
+@app.route('/how are you')
 def hello():
-    return "i am good how about you"
-
+  return 'i am good how about you'
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+  app.run(host="0.0.0.0", port=8080)
